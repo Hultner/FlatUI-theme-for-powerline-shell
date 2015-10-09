6c7f9de2ce9b72b4cae26eb12ff3a3d5cf743f23
Alexander Hultnér <alexander.hultner@heliospectra.com>
2015-10-09 12:12:04 +0200

    Added a light colour scheme as well

--- flat-light.py
+++ flat-light.py
@@ -1,15 +1,15 @@
 class Color(DefaultColor):
-    USERNAME_FG = 15
-    USERNAME_BG = 0
+    USERNAME_FG = 0
+    USERNAME_BG = 15
     USERNAME_ROOT_BG = 1
 
     HOSTNAME_FG = 0
     HOSTNAME_BG = 14
 
     HOME_SPECIAL_DISPLAY = False
-    PATH_FG = 7
-    PATH_BG = 0
-    CWD_FG = 15
+    PATH_FG = 0
+    PATH_BG = 15
+    CWD_FG = 0
     SEPARATOR_FG = 14
 
     READONLY_BG = 1
@@ -23,13 +23,13 @@ class Color(DefaultColor):
     JOBS_FG = 0
     JOBS_BG = 8
 
-    CMD_PASSED_FG = 15
-    CMD_PASSED_BG = 0
-    CMD_FAILED_FG = 15
-    CMD_FAILED_BG = 1
+    CMD_PASSED_FG = 0
+    CMD_PASSED_BG = 14
+    CMD_FAILED_FG = 1
+    CMD_FAILED_BG = 15
 
     SVN_CHANGES_FG = REPO_DIRTY_FG
     SVN_CHANGES_BG = REPO_DIRTY_BG
 
-    VIRTUAL_ENV_BG = 15
-    VIRTUAL_ENV_FG = 2
+    VIRTUAL_ENV_BG = 2
+    VIRTUAL_ENV_FG = 15
